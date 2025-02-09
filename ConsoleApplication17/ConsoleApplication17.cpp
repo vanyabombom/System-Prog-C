30839236b098818ce9c2461c5bb228cea7524a1f
vanyabombom <vanyadondov@gmail.com>
2025-02-09 17:15:44 +0200

    done2

--- ConsoleApplication17/ConsoleApplication17.cpp
+++ ConsoleApplication17/ConsoleApplication17.cpp
@@ -3,27 +3,32 @@
 #include <Windows.h>
 #include <string>
 #include <tchar.h>
-
 using namespace std;
 
-void vowelsWordCounter(_TCHAR* text) {
-    _TCHAR vowels[] = { 'a', 'e' , 'o' , 'u' , 'i', 'y', 'f', 'g' };
+int vowelsCounter(_TCHAR* text) {
+    _TCHAR vowels[] = { 'a', 'e' , 'o' , 'u' , 'i', 'y'};
     int counter = 0;
-    for (int i = 0; i < _tcslen(text); i++) {
-        if ((text[i] != ' ' and text[i + 1] != ' ') or (i == _tcslen(text) - 1 and text[i] != ' ')) {
-            for (_TCHAR c : vowels) {
-                if (text[i] == c) counter++;
+    for (int i = 0; i < _tcslen(text); i++)
+    {
+        if ((text[i] != ' ' && text[i + 1] != ' ') || (i == _tcslen(text) - 1 && text[i] != ' ')) 
+        {
+            for (_TCHAR symbol : vowels) 
+            {
+                if (text[i] == symbol)
+                {
+                    counter++;
+                }
             }
         }
     }
-    cout << counter << endl;
+    return counter;
 }
 
-int main() {
-    setlocale(LC_ALL, "");
+int main() 
+{
+    _TCHAR text[100] = _TEXT("");
+    _fgetts(text, 100, stdin);
 
-    _TCHAR text[256] = _TEXT("");
-    _fgetts(text, 256, stdin);
-
-    vowelsWordCounter(text);
+    int counter = vowelsCounter(text);
+    cout << counter;
 }
