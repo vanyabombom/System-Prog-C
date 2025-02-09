1daf51776d2c29a7f5664c43ea3077b45680898d
vanyabombom <vanyadondov@gmail.com>
2025-02-09 17:13:13 +0200

    Done

---
+++ ConsoleApplication17/ConsoleApplication17.cpp
@@ -0,0 +1,29 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <Windows.h>
+#include <string>
+#include <tchar.h>
+
+using namespace std;
+
+void vowelsWordCounter(_TCHAR* text) {
+    _TCHAR vowels[] = { 'a', 'e' , 'o' , 'u' , 'i', 'y', 'f', 'g' };
+    int counter = 0;
+    for (int i = 0; i < _tcslen(text); i++) {
+        if ((text[i] != ' ' and text[i + 1] != ' ') or (i == _tcslen(text) - 1 and text[i] != ' ')) {
+            for (_TCHAR c : vowels) {
+                if (text[i] == c) counter++;
+            }
+        }
+    }
+    cout << counter << endl;
+}
+
+int main() {
+    setlocale(LC_ALL, "");
+
+    _TCHAR text[256] = _TEXT("");
+    _fgetts(text, 256, stdin);
+
+    vowelsWordCounter(text);
+}
